b49443f158c97cd1c84934bba0e8ffea2f9356f3
Har5haw <harshavardhan.gadireddy@zemosolabs.com>
2021-03-22 23:47:34 +0530

    bugs fixed

--- Part_4_Finding_Web_Links/Driver.java
+++ Part_4_Finding_Web_Links/Driver.java
@@ -3,6 +3,6 @@ package Part_4_Finding_Web_Links;
 public class Driver {
     public static void main(String[] args) {
         Part4 part4 = new Part4();
-        part4.readFile();
+        part4.readUrl();
     }
 }
